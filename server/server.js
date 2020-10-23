31b3620aa4fa335a45900f9b54dd6819e82fb2c3
Johan Rodriguez <johan.triana@tecnofactory.com.co>
2020-10-23 18:42:34 -0500

    RestServer - usuario

--- server/server.js
+++ server/server.js
@@ -1,6 +1,9 @@
 require('./config/config')
 
 const express = require('express')
+
+const mongoose = require('mongoose');
+
 const app = express()
 const bodyParser = require('body-parser')
  
@@ -10,40 +13,14 @@ app.use(bodyParser.urlencoded({ extended: false }))
 // parse application/json
 app.use(bodyParser.json())
 
-app.get('/usuario', function (req, res) {
-  res.json('get Usuario')
-})
+app.use(require('./routes/usuario'))
 
-app.post('/usuario', function (req, res) {
-  
-  let body = req.body
+mongoose.connect(process.env.URLDB, 
+                { useNewUrlParser: true, useCreateIndex: true },
+                (err, res) => {
+  if(err) throw err
 
-  if (body.nombre === undefined) {
-
-    res.status(400).json({
-      ok: false,
-      mensaje: 'El nombre es necesario'
-    })
-  } else {
-    
-    res.json({
-      body
-    })
-  }
-
-
-})
-
-app.put('/usuario/:id', function (req, res) {
-  let id = req.params.id;
-
-  res.json({
-    id
-  })
-})
-
-app.delete('/usuario', function (req, res) {
-  res.json('delete Usuario')
+  console.log('Base de datos ONLINE!')
 })
  
 app.listen(process.env.PORT, () => console.log('Escuchando en el puerto: ', 3000))
