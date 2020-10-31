ecccc9375f27a31fc1fd543408b275c5ac2b9d0a
Johan Rodriguez <johan.triana@tecnofactory.com.co>
2020-10-30 21:48:40 -0500

    login normal

---
+++ server/middlewares/autenticacion.js
@@ -0,0 +1,45 @@
+const jwt = require('jsonwebtoken')
+
+// =========================
+// Verificar Token
+// =========================
+let verificaToken = (req, res, next) => {
+
+  let token = req.get('token')
+
+  jwt.verify(token, process.env.SEED, (err, decoded) => {
+
+    if (err) {
+      return res.status(401).json({
+        ok: false,
+        err
+      })
+    }
+
+    req.usuario = decoded.usuario
+    next()
+
+  })
+}
+
+let verificaAdminRole = (req, res, next) => {
+
+  let usuario = req.usuario
+
+  if (usuario.role !== 'ADMIN_ROLE') {
+    return res.status(401).json({
+      ok: false,
+      err: {
+        mensaje: 'No tiene permisos'
+      }
+    })
+  }
+
+  next()
+}
+
+
+module.exports = {
+  verificaToken,
+  verificaAdminRole
+}
